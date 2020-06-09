766b9a2ab7d7612c10cc974227a41694d318a4b6
Dmitry <megagugl530@gmail.com>
2020-06-09 20:15:34 +0300

    Почистил код

--- Gulp-start-template/gulp/function/getArraysDiff.js
+++
@@ -1,5 +0,0 @@
-function getArraysDiff(a1, a2) {
-    return a1.filter(i=>!a2.includes(i)).concat(a2.filter(i=>!a1.includes(i)))
-}
-
-exports.getArraysDiff = getArraysDiff;
--- Gulp-start-template/gulp/tasks/clearBuildDir.js
+++
@@ -1,8 +0,0 @@
-import {del, config} from "../../config";
-
-function clearBuildDir() {
-        return del(config.build.dir)
-}
-
-exports.clearBuildDir = clearBuildDir;
-
